3086927d5cc8e082d5a8126aed0ceefe01011cb0
Héliton Oliveira <heliton.oliveira88@gmail.com>
2021-04-20 13:26:36 -0300

    :recycle: refactory: refactory on global style

---
+++ src/styles/theme.ts
@@ -0,0 +1,27 @@
+export default {
+  size: {
+    FONT_SIZE_LARGE: 32,
+    FONT_SIZE_MEDIUM: 24,
+    FONT_SIZE_NORMAL: 17,
+    FONT_SIZE_SMALL: 15,
+    FONT_SIZE_SUPER_SMALL: 13,
+  },
+
+  fonts: {
+    REGULAR: 'Jost_400Regular',
+    MEDIUM: 'Jost_500Medium',
+    SEMIBOLD: 'Jost_600SemiBold',
+  },
+
+  colors: {
+    WHITE: '#FFFFFF',
+    LIGHT_GRAY: '#F5FAF7',
+    GRAY: '#AAB2AD',
+    GRAY_MEDIUM: '#5C6660',
+    GRAY_DARK: '#52665A',
+
+    GRENN_LIGHT: '#DAF2E4',
+    GRENN: '#32B768',
+    GRENN_DARK: '#2B7A4B',
+  },
+};
